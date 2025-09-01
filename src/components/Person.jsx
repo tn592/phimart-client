0d797aefd9b322bf247e7a0e6dce055136f0162f
tanzina <tn59249@yahoo.com>
2025-09-01 12:25:48 +0600

    29.2 done

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,15 +1,43 @@
 import { useState } from "react";
 const Person = () => {
-	// ["Something", false]	we can't define useState inside any if block
-	const [person, setPerson] = useState("Something");
-	// const [visible, setVisible] = useState(false);
+	// const personObj = {
+	// 	firstName: "Mr.",
+	// 	lastName: "X",
+	// 	email: "mail@example.com"
+	// };
+	// const [firstName, setFirstName] = useState("")
+	// const [lastName, setLastName] = useState("")
+	// const [person, setPerson] = useState(personObj);
+	const [person, setPerson] = useState({
+		firstName: "Mr.",
+		lastName: "X",
+		email: "mail@example.com",
+		address: {
+			city: "Mirpur",
+			state: "Dhaka"
+		}
+	});
 	const handleClick = () => {
-		setPerson("John");
-		console.log(person);
+		// setFirstName("Mr.");
+		// setLastName("X");
+		// const newPerson = { firstName: "John", lastName: "Doe", email: "mail@example.com"};
+		const newPerson = {
+			...person,
+			// firstName: "John",
+			// lastName: "Doe",
+			address:{
+				...person.address, city: "Dhanmondi"
+			}
+		};
+		setPerson(newPerson);
 	};
-	console.log(person)
 	return (
 		<div className="m-5">
+			{/*<h1>{firstName} {lastName}</h1>*/}
+			<h1>
+				{person.firstName} {person.lastName} {person.email}
+			</h1>
+			<p>{person.address.city}, {person.address.state}</p>
 			<button
 				onClick={handleClick}
 				className="px-3 py-2 bg-blue-500 text-white rounded-sm"
