dc6ce46e490e161f9015910129273d2cedea8b0b
tanzina <tn59249@yahoo.com>
2025-09-01 22:23:37 +0600

    29.5 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 // import "./App.css";
-import Employee from "./components/Employee";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
 function App() {
+  const [cartItems, setCartItems] = useState([
+    "product 1",
+    "product 2",
+    "product 3",
+    "product 4",
+  ]);
+  const handleProductRemove = (item) => {
+    setCartItems(cartItems.filter((product) => product != item));
+  };
   return (
-    <>
-      <Employee />
-    </>
+    <div className="m-5">
+      <Navbar cartItemsCount={cartItems.length} />
+      <Cart
+        cartItems={cartItems}
+        onClear={() => setCartItems([])}
+        onRemove={handleProductRemove}
+      />
+    </div>
   );
 }
 
