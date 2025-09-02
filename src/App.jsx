b7d45a7e8edc2270be854bc0496309dc7477f442
tanzina <tn59249@yahoo.com>
2025-09-02 11:24:07 +0600

    29.7 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // import "./App.css";
 import Effect from "./components/Effect";
 function App() {
-  return (<div className="m-5">
-    <Effect />
-    <Effect />
-    <Effect />
-  </div>);
+  return (
+    <div className="m-5">
+      <Effect />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,14 +1,29 @@
-import { useEffect } from "react";
-
-let count = 0;
+import { useEffect, useState } from "react";
 
 const Effect = () => {
+	const [users, setUsers] = useState([]);
+	const [dependancyA, setDependancyA] = useState(0);
+	const [dependancyB, setDependancyB] = useState(0);
+
 	useEffect(() => {
 		console.log("Effect Occured");
-	});
+		setUsers(["John"]);
+	}, [dependancyA, dependancyB]);
 	return (
 		<div>
-			<h1>UserList {++count} </h1>
+			<h1>UserList</h1>
+			<button
+				onClick={() => setDependancyA(Math.random())}
+				className="px-3 py-2 bg-blue-500 me-2"
+			>
+				Click Me A
+			</button>
+			<button
+				onClick={() => setDependancyB(Math.random())}
+				className="px-3 py-2 bg-blue-500"
+			>
+				Click Me B
+			</button>
 		</div>
 	);
 };
