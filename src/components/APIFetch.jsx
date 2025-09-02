6ee570e3764430516693d715d7f80028618f858a
tanzina <tn59249@yahoo.com>
2025-09-02 12:15:44 +0600

    module 29 done

--- src/components/APIFetch.jsx
+++ src/components/APIFetch.jsx
@@ -1,14 +1,24 @@
 import { useState, useEffect } from "react";
-
+import axios from "axios";
 const APIFetch = () => {
 	const [users, setUsers] = useState([]);
+	const [error, setError] = useState("");
+	// useEffect(() => {
+	// 	fetch("https://jsonplaceholder.typicode.com/users")
+	// 		.then((res) => res.json())
+	// 		.then((data) => setUsers(data));
+	// }, []);
+
 	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((res) => res.json())
-			.then((data) => setUsers(data));
+		axios
+			.get("https://jsonplaceholder.typicode.com/users")
+			.then((data) => setUsers(data.data))
+			.catch((err) => setError(err.message));
 	}, []);
+
 	return (
 		<div>
+			{error && <p className="bg-red-100 text-red-900"> {error} </p>}
 			<p>
 				<ul className="list-disc m-5">
 					{users.map((user) => (
