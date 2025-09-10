78334614ffac881c33dd1d32d716e19891238832
tanzina <tn59249@yahoo.com>
2025-09-10 20:58:59 +0600

    31.9 done

--- src/components/Carousel/CarouselSlide.jsx
+++ src/components/Carousel/CarouselSlide.jsx
@@ -1,18 +1,18 @@
-import book from "../../assets/images/book.png";
 import bgImg from "../../assets/images/banner-image-bg.jpg";
-const CarouselSlide = () => {
+// eslint-disable-next-line react/prop-types
+const CarouselSlide = ({title, subtitle, image}) => {
 	return (
-		<section className="w-full h-[650px]">
-			<div
-				style={{ backgroundImage: `url(${bgImg})` }}
-				className="max-w-6xl flex items-center justify-between px-8"
-			>
+		<section
+			className="w-full h-[650px] bg-cover bg-center flex justify-center item-center px-8"
+			style={{ backgroundImage: `url(${bgImg})` }}
+		>
+			<div className="max-w-6xl flex items-center justify-between px-8">
 				{/*Left Content*/}
 				<div className="w-1/2">
 					<h1 className="text-5xl font-bold text-gray-900">
-						The Fine Print Book Collections
+						{title}
 					</h1>
-					<p className="text-gray-600 my-4">This is heading</p>
+					<p className="text-gray-600 my-4">{subtitle}</p>
 					<button className="btn btn-secondary px-6 py-3 rounded-full shadow-md">
 						Shop Product
 					</button>
@@ -22,7 +22,7 @@ const CarouselSlide = () => {
 				<div className="w-1/2 flex justify-center">
 					<img
 						className="max-w-md drop-shadow-lg"
-						src={book}
+						src={image}
 						alt=""
 					/>
 				</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import CarouselSlide from "../components/Carousel/CarouselSlide";
+import HeroCarousel from "../components/Carousel/HeroCarousel";
 
 const Home = () => {
 	return (
 		<div>
-			<CarouselSlide />
+			<HeroCarousel />
 		</div>
 	);
 };
