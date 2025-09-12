ff32891d8c8030d8f6c1dfba298b13a6d616456a
tanzina <tn59249@yahoo.com>
2025-09-12 22:54:37 +0600

    33.2 done

--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -1,22 +1,39 @@
 import { useEffect, useState } from "react";
 import apiClient from "../../services/api-client";
 import ProductList from "./ProductList";
+import Pagination from "./Pagination";
 
 const ShopPage = () => {
 	const [products, setProducts] = useState([]);
 	const [loading, setLoading] = useState(false);
+	const [totalPages, setTotalPages] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
 
 	useEffect(() => {
+		fetchProducts();
+	}, []);
+
+	const fetchProducts = () => {
 		setLoading(true);
 		apiClient
 			.get("/products")
-			.then((res) => setProducts(res.data.results))
+			.then((res) => {
+				setProducts(res.data.results);
+				setTotalPages(
+					Math.ceil(res.data.count / res.data.results.length),
+				);
+			})
 			.catch((error) => console.log(error))
 			.finally(() => setLoading(false));
-	}, []);
+	};
 	return (
 		<div>
 			<ProductList products={products} loading={loading} />
+			<Pagination
+				totalPages={totalPages}
+				currentPage={currentPage}
+				handlePageChange={setCurrentPage}
+			/>
 		</div>
 	);
 };
