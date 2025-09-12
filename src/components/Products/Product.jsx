388053fe41a29de30c6f79f9556f011cf65b9440
tanzina <tn59249@yahoo.com>
2025-09-12 20:34:28 +0600

    module 32 done

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -22,55 +22,60 @@ const Product = () => {
 			.finally(() => setLoading(false));
 	}, []);
 	return (
-		<section className="mx-auto py-16 bg-gray-50">
-			<div className="flex justify-between items-center px-4 md:px-8 mb-4">
-				<h2 className="text-3xl md:text-4xl font-bold">
-					Trending Product
-				</h2>
-				<a
-					href="#"
-					className="btn btn-secondary px-6 py-6 rounded-full text-lg"
-				>
-					View All
-				</a>
-			</div>
-			{/*Spinner*/}
-			{isLoading && (
-				<div className="flex justify-center items-center py-10">
-					<span className="loading loading-spinner text-secondary loading-xl"></span>
+		<section className="bg-gray-50">
+			<div className="py-12 px-4 max-w-7xl mx-auto">
+				<div className="flex justify-between items-center px-4 md:px-8 mb-4">
+					<h2 className="text-3xl md:text-4xl font-bold">
+						Trending Product
+					</h2>
+					<a
+						href="#"
+						className="btn btn-secondary px-6 py-6 rounded-full text-lg"
+					>
+						View All
+					</a>
 				</div>
-			)}
+				{/*Spinner*/}
+				{isLoading && (
+					<div className="flex justify-center items-center py-10">
+						<span className="loading loading-spinner text-secondary loading-xl"></span>
+					</div>
+				)}
 
-			{error && <ErrorAlert error={error} />}
+				{error && <ErrorAlert error={error} />}
 
-			{/*Product Slider*/}
-			{!isLoading && !error && products.length > 0 && (
-				<Swiper
-					modules={[Navigation]}
-					spaceBetween={10}
-					slidesPerView={1}
-					navigation
-					breakpoints={{
-						640: { slidesPerView: 2 },
-						1024: { slidesPerView: 3 },
-					}}
-					className="mt-4 px-4 container"
-				>
-					{products.map((product) => (
-						<SwiperSlide
-							key={product.id}
-							className="flex justify-center"
-						>
-							<ProductItem key={product.id} product={product} />
-						</SwiperSlide>
-					))}
-				</Swiper>
-			)}
-			{!isLoading && !error && products.length === 0 && (
-				<p className="text-center text-gray-500 mt-6">
-					No Products Available
-				</p>
-			)}
+				{/*Product Slider*/}
+				{!isLoading && !error && products.length > 0 && (
+					<Swiper
+						modules={[Navigation]}
+						spaceBetween={10}
+						slidesPerView={1}
+						navigation
+						breakpoints={{
+							640: { slidesPerView: 2 },
+							1024: { slidesPerView: 3 },
+						}}
+						className="mt-4 px-4 container"
+					>
+						{products.map((product) => (
+							<SwiperSlide
+								key={product.id}
+								className="flex justify-center"
+							>
+								<ProductItem
+									key={product.id}
+									product={product}
+								/>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				)}
+				{!isLoading && !error && products.length === 0 && (
+					<p className="text-center text-gray-500 mt-6">
+						No Products Available
+					</p>
+				)}
+			</div>
 		</section>
 	);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import HeroCarousel from "../components/Carousel/HeroCarousel";
+import Category from "../components/Categories/Category";
 import DiscountSection from "../components/Discount/DiscountSection";
 import Features from "../components/Features";
 import Product from "../components/Products/Product";
@@ -8,6 +9,7 @@ const Home = () => {
 		<div>
 			<HeroCarousel />
 			<Features />
+			<Category />
 			<Product />
 			<DiscountSection />
 		</div>
