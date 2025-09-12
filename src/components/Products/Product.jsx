ed79d88cc72a1736e1519897dcd9600ede82be0b
tanzina <tn59249@yahoo.com>
2025-09-12 10:56:54 +0600

    32.3 done

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,17 +6,24 @@ import { Navigation } from "swiper/modules";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import ErrorAlert from "../ErrorAlert";
+import apiClient from "../../services/api-client";
 
 const Product = () => {
 	const [products, setProducts] = useState([]);
+	const [isLoading, setLoading] = useState(false);
+	const [error, setError] = useState("");
 	useEffect(() => {
-		axios
-			.get("https://phi-mart-tau.vercel.app/api/v1/products")
-			.then((res) => setProducts(res.data.results));
+		setLoading(true);
+		apiClient
+			.get("/products/")
+			.then((res) => setProducts(res.data.results))
+			.catch((err) => setError(err.message))
+			.finally(() => setLoading(false));
 	}, []);
 	return (
 		<section className="mx-auto py-16 bg-gray-50">
-			<div className="flex justify-between items-center px-4 md:px-8">
+			<div className="flex justify-between items-center px-4 md:px-8 mb-4">
 				<h2 className="text-3xl md:text-4xl font-bold">
 					Trending Product
 				</h2>
@@ -28,27 +34,43 @@ const Product = () => {
 					View All
 				</a>
 			</div>
+			{/*Spinner*/}
+			{isLoading && (
+				<div className="flex justify-center items-center py-10">
+					<span className="loading loading-spinner text-secondary loading-xl"></span>
+				</div>
+			)}
+
+			{error && <ErrorAlert error={error} />}
+
 			{/*Product Slider*/}
-			<Swiper
-				modules={[Navigation]}
-				spaceBetween={10}
-				slidesPerView={1}
-				navigation
-				breakpoints={{
-					640: { slidesPerView: 2 },
-					1024: { slidesPerView: 3 },
-				}}
-				className="mt-4 px-4 container"
-			>
-				{products.map((product) => (
-					<SwiperSlide
-						key={product.id}
-						className="flex justify-center"
-					>
-						<ProductItem key={product.id} product={product} />
-					</SwiperSlide>
-				))}
-			</Swiper>
+			{!isLoading && !error && products.length > 0 && (
+				<Swiper
+					modules={[Navigation]}
+					spaceBetween={10}
+					slidesPerView={1}
+					navigation
+					breakpoints={{
+						640: { slidesPerView: 2 },
+						1024: { slidesPerView: 3 },
+					}}
+					className="mt-4 px-4 container"
+				>
+					{products.map((product) => (
+						<SwiperSlide
+							key={product.id}
+							className="flex justify-center"
+						>
+							<ProductItem key={product.id} product={product} />
+						</SwiperSlide>
+					))}
+				</Swiper>
+			)}
+			{!isLoading && !error && products.length === 0 && (
+				<p className="text-center text-gray-500 mt-6">
+					No Products Available
+				</p>
+			)}
 		</section>
 	);
 };
---
+++ src/services/api-client.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+	baseURL: "https://phi-mart-tau.vercel.app/api/v1",
+});
