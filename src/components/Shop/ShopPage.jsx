cdaa3ca0582f3e4c262e2739b8b92a2046dbde4a
tanzina <tn59249@yahoo.com>
2025-09-13 19:20:54 +0600

    33.6 done

--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -2,13 +2,29 @@ import ProductList from "./ProductList";
 import Pagination from "./Pagination";
 import { useState } from "react";
 import useFetchProduct from "../../hooks/useFetchProducts";
+import FilterSection from "./FilterSection";
 
 const ShopPage = () => {
 	const [currentPage, setCurrentPage] = useState(1);
+	const [priceRange, setPriceRange] = useState([0, 1000]);
 	const { products, loading, totalPages } = useFetchProduct(currentPage);
 
+	const handlePriceChange = (index, value) => {
+		setPriceRange((prev) => {
+			const newRange = [...prev];
+			newRange[index] = value;
+			return newRange;
+		});
+		setCurrentPage(1);
+	};
+
 	return (
-		<div>
+		<div className="max-w-7xl mx-auto px-4 py-8">
+			<h1 className="text-3xl font-bold mb-8">Shop Our Products</h1>
+			<FilterSection
+				priceRange={priceRange}
+				handlePriceChange={handlePriceChange}
+			/>
 			<ProductList products={products} loading={loading} />
 			<Pagination
 				totalPages={totalPages}
