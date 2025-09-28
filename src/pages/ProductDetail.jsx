a1d10c0386c2b3da7b281076a3728df94b7cb4eb
tanzina <tn59249@yahoo.com>
2025-09-28 12:42:57 +0600

    34.3 done

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,3 +1,4 @@
+import AddToCartButton from "../components/ProductDetails/AddToCartButton";
 import ProductImageGallery from "../components/ProductDetails/ProductImageGallery";
 
 const ProductDetail = () => {
@@ -21,11 +22,16 @@ const ProductDetail = () => {
 		],
 	};
 	return (
-		<div>
-			<ProductImageGallery
-				images={product.images}
-				ProductName={product.name}
-			/>
+		<div className="w-3/4 mx-auto px-4 py-8">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12">
+				<ProductImageGallery
+					images={product.images}
+					ProductName={product.name}
+				/>
+				<div className="mt-auto">
+					<AddToCartButton product={product} />
+				</div>
+			</div>
 		</div>
 	);
 };
